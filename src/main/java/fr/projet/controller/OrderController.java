14cdc04a87ffeffcfa8dcc669ce5dad97205eb0e
Hey <mounaim.laghnimi@gmail.com>
2019-10-16 17:56:34 +0200

    fix

--- src/main/java/fr/projet/controller/OrderController.java
+++ src/main/java/fr/projet/controller/OrderController.java
@@ -25,14 +25,14 @@ import fr.projet.services.OrderService;
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
-	
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@PreAuthorize("hasAuthority('C_ORDER')")
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public Order create(@RequestBody Order order) { //throws BadRequestException {
+	public Order create(@RequestBody Order order) { // throws BadRequestException {
 		try {
 			return orderService.save(order);
 		} catch (BadRequestException e) {
@@ -40,75 +40,68 @@ public class OrderController {
 			return null;
 		}
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Order findById(@PathVariable Long id) {
 		return orderService.find(id);
 	}
-	
 
 	@PreAuthorize("hasRole('ADMIN') or hasRole('CUSTOMER')")
- 	@PostAuthorize("hasRole('ADMIN') or returnObject.user.email == principal.username")
+	@PostAuthorize("hasRole('ADMIN') or returnObject.user.email == principal.username")
 	@RequestMapping(value = "/orderNumber/{orderNumber}", method = RequestMethod.GET)
 	public Order findOneByOrderNumber(@PathVariable String orderNumber) {
 		Order o = orderService.findOneByNumber(orderNumber);
-		System.out.println(o);	
+		System.out.println(o);
 		return o;
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
- 	@PostAuthorize("hasRole('ADMIN')")
+	@PostAuthorize("hasRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Order> findAll() {
 		List<Order> orders = orderService.findAll();
-		
-		System.out.println(orders);	
-	
+
+		System.out.println(orders);
+
 		return orders;
 	}
-	
- 	@PreAuthorize("hasRole('ADMIN') or (hasRole('CUSTOMER') and  @securityService.isConnectedUser(#id))")
+
+	@PreAuthorize("hasRole('ADMIN') or (hasRole('CUSTOMER') and  @securityService.isConnectedUser(#id))")
 	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
 	public List<Order> findAllOrdersByUserEmail(@PathVariable Long id) {
 		List<Order> orders = orderService.findAllOrdersByUserId(id);
-		
-		System.out.println(orders);	
-		System.out.println("############################################################");	
+
+		System.out.println(orders);
+		System.out.println("############################################################");
 		return orders;
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN') or hasRole('CUSTOMER')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public Order update(@PathVariable Long id, 
-			@RequestBody Order order) {
+	public Order update(@PathVariable Long id, @RequestBody Order order) {
 		order.setId(id);
 		return orderService.update(order);
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
 	@PostAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public Order delete(@PathVariable Long id) {
 		return orderService.delete(id);
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN') or hasRole('CUSTOMER')")
- 	@PostAuthorize("hasRole('ADMIN') or returnObject.customer.email == principal.username")
-	@RequestMapping(value = "/search", method = RequestMethod.GET) 
-	public List<Order> search(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
-			@RequestParam(required = false) String orderNumber,
-			@RequestParam(required = false) Integer totalPrice
-			) {
-		
+	@PostAuthorize("hasRole('ADMIN') or returnObject.customer.email == principal.username")
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public List<Order> search(
+			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
+			@RequestParam(required = false) String orderNumber, @RequestParam(required = false) Integer totalPrice) {
 
-		
 		OrderCriteria criteria = new OrderCriteria(purchaseDate, orderNumber, totalPrice);
-		
+
 		return orderService.search(criteria);
-		
+
 	}
-
-
 
 }
