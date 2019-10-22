48a8dfd0387359a24e3348e15916f72e8a40c7fc
VegetarianGlobeTrotter <mounaim.laghnimi@gmail.com>
2019-10-23 01:03:01 +0200

    fix product pages front-end

--- embryo-angular-8/src/app/Pages/Products/ProductsList/ProductsList.component.ts
+++ embryo-angular-8/src/app/Pages/Products/ProductsList/ProductsList.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { Router, ActivatedRoute, Params }   from '@angular/router';
 import { Observable ,  Subscription } from 'rxjs';
 
 import { EmbryoService } from '../../../Services/Embryo.service';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
+import { AdminPanelServiceService } from 'src/app/AdminPanel/Service/AdminPanelService.service';
 
 @Component({
   selector: 'app-ProductsList',
@@ -23,7 +26,8 @@ export class ProductsListComponent implements OnInit {
    constructor(private route: ActivatedRoute,
                private router: Router, 
                public embryoService : EmbryoService,
-               ) {
+               public translate : TranslateService,
+					private adminPanelService : AdminPanelServiceService) {
    }
 
    ngOnInit() {
@@ -36,6 +40,12 @@ export class ProductsListComponent implements OnInit {
             this.getPageTitle();
          });   
       });
+      this.adminPanelService.getProducts().valueChanges().subscribe(res => this.getProductResponse(res));
+
+   }
+
+   public searchByName(){
+      
    }
 
    public getPageTitle() {
@@ -83,4 +93,75 @@ export class ProductsListComponent implements OnInit {
       });
       return hits;
    }
+
+
+	productsList 		      : any;
+	productsGrid 			   : any;
+	popUpDeleteUserResponse : any;
+	showType	    				: string = 'grid';
+	displayedProductColumns : string [] = ['id', 'image','name','brand','category', 'product_code', 'discount_price', 'price','action' ];
+	@ViewChild(MatPaginator,{static: false}) paginator : MatPaginator;
+	@ViewChild(MatSort,{static: false}) sort           : MatSort;
+
+	//getProductResponse method is used to get the response of all products.
+ 	public getProductResponse(response) {
+      this.productsGrid = null;
+      let products = ((response.men.concat(response.women)).concat(response.gadgets)).concat(response.accessories);
+      this.productsGrid = products;
+   }
+
+  	/**
+	  * productShowType method is used to select the show type of product.
+	  */
+	productShowType(type) {
+		this.showType = type;
+		if(type == 'list'){
+			document.getElementById('list').classList.add("active");
+			document.getElementById('grid').classList.remove('active');
+			this.productsList = new MatTableDataSource(this.productsGrid);
+			setTimeout(()=>{
+				this.productsList.paginator = this.paginator;
+				this.productsList.sort = this.sort;
+			},0)
+			
+		}
+		else{
+			document.getElementById('grid').classList.add("active");
+			document.getElementById('list').classList.remove('active');
+		}
+	}
+
+	/**
+	  * onEditProduct method is used to open the edit page and edit the product.
+	  */
+	onEditProduct(data){
+		this.router.navigate(['/admin-panel/product-edit',data.type,data.id]);
+		this.adminPanelService.editProductData = data;
+	}
+
+	/* 
+     *deleteProduct method is used to open a delete dialog.
+     */
+   deleteProduct(i){
+      this.adminPanelService.deleteDialog("Are you sure you want to delete this product permanently?").
+         subscribe( res => {this.popUpDeleteUserResponse = res},
+                    err => console.log(err),
+                    ()  => this.getDeleteResponse(this.popUpDeleteUserResponse,i))
+   }
+
+   /**
+     * getDeleteResponse method is used to delete a product from the product list.
+     */
+   getDeleteResponse(response : string,i){
+      if(response == "yes"){
+      	if(this.showType == 'grid') {
+         	this.productsGrid.splice(i,1);
+      	}else if(this.showType == 'list'){
+				this.productsList.data.splice(i,1);
+				this.productsList = new MatTableDataSource(this.productsList.data);
+      		this.productsList.paginator = this.paginator;
+      	}
+      }
+   }
+
 }
